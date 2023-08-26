277b901be8610a10f1fb67a81cfd717696f01b04
Bozheng Liu <lb12016297@gmail.com>
2023-08-26 11:07:03 -0500

    重新引入熊包
    重新加入熊包及其对应选项,
    修改代码结构, 增强可读及其维护性
    修改小bug

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@
 name = "保鲜/反鲜 Perish Time Modifier"
 description = "Change perish time for mushroom light/icebox/saltbox/endtable"
 author = "MicheaBoab"
-version = "2.2"
+version = "3.0"
 
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
 
@@ -10,7 +10,7 @@ api_version = 10
 
 dst_compatible = true
 
--- Can specify a custom icon for this mod!
+-- custom icon for this mod!
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 
@@ -23,48 +23,26 @@ client_only_mod = false
 server_filter_tags = {}
 
 configuration_options = 
-{
-	--蘑菇灯
-	{
-		name = "",
-		label = "蘑菇灯 󰀏",
-		hover = "",
-		default = 0,
-		options = {
-			{description = "", data = 0},
-		},
-	},
-	{
-		name = "mushroom_light_config",
-		label = "腐烂速率",
-		hover = "Mushroom Light spoilage rate config",
-		options =	{
-			{description = "默认腐烂速度", data = 0.25, hover = "Default spoil time"},
-			{description = "长时间保鲜", data = 0.05, hover = "Spoil way slower"},
-			{description = "完全保鲜", data = 0, hover = "Food fresh forever"},
-			{description = "神奇的反鲜", data = - 2, hover = "Regain freshness over time"},
-		},
-		default = 0.25,
-	},
-	
+{	
 	--冰箱
 	{
 		name = "",
-		label = "冰箱 󰀏",
-		hover = "",
+		label = "󰀏 Fridge	冰箱 󰀏",
+		hover = "Fridge",
 		default = 0,
 		options = {
 			{description = "", data = 0},
 		},
 	},
 		{
-		name = "icebox_config",
-		label = "腐烂速率",
-		hover = "Ice Box spoilage rate config",
+		name = "fridge_config",
+		label = "spoilage rate config	腐烂速率",
+		hover = "Ice Fridge spoilage rate config",
 		options =	{
-			{description = "默认腐烂速度", data = 0.25, hover = "Default spoil time"},
-			{description = "长时间保鲜", data = 0.05, hover = "Spoil way slower"},
-			{description = "完全保鲜", data = 0, hover = "Food fresh forever"},
+			{description = "默认腐烂速度", data = 0.5, hover = "Default spoil time"},
+			-- 这个数值改为小于等于0的时候会影响熊包的正常工作. 暂时改为超长时间约等于完全保鲜
+			{description = "完全保鲜(超长时间)", data = 0.0000000005, hover = "Spoil slower"},
+			--{description = "完全保鲜", data = 0, hover = "Food fresh forever(keep current freshness)"},
 			{description = "神奇的反鲜", data = - 2, hover = "Regain freshness over time"},
 		},
 		default = 0.5,
@@ -73,8 +51,8 @@ configuration_options =
 	--盐盒
 	{
 		name = "",
-		label = "盐盒 󰀏",
-		hover = "",
+		label = "󰀏 Salt Box	盐盒 󰀏",
+		hover = "Salt Box",
 		default = 0,
 		options = {
 			{description = "", data = 0},
@@ -82,33 +60,45 @@ configuration_options =
 	},
 	{
 		name = "saltbox_config",
-		label = "腐烂速率",
+		label = "spoilage rate config	腐烂速率",
 		hover = "Salt Box spoilage rate config",
 		options =	{
 			{description = "默认腐烂速度", data = 0.25, hover = "Default spoil time"},
-			{description = "长时间保鲜", data = 0.05, hover = "Spoil way slower"},
-			{description = "完全保鲜", data = 0, hover = "Food fresh forever"},
+			{description = "长时间保鲜", data = 0.05, hover = "Spoil slower"},
+			{description = "完全保鲜", data = 0, hover = "Food fresh forever(keep current freshness)"},
 			{description = "神奇的反鲜", data = - 2, hover = "Regain freshness over time"},
 		},
 		default = 0.25,
 	},
 	
-	--各种背包
-	--[[
+	--蘑菇灯
 	{
 		name = "",
-		label = "施工中...",
-		hover = "",
+		label = "󰀏 Mushroom_Light	蘑菇灯 󰀏",
+		hover = "Mushroom_Light",
 		default = 0,
 		options = {
 			{description = "", data = 0},
 		},
 	},
-	]]--
+	{
+		name = "mushroom_light_config",
+		label = "spoilage rate config	蘑菇灯",
+		hover = "Mushroom Light spoilage rate config",
+		options =	{
+			{description = "默认腐烂速度", data = 0.25, hover = "Default spoil time"},
+			{description = "长时间保鲜", data = 0.05, hover = "Spoil slower"},
+			{description = "完全保鲜", data = 0, hover = "Food fresh forever(keep current freshness)"},
+			{description = "神奇的反鲜", data = - 2, hover = "Regain freshness over time"},
+		},
+		default = .25,
+	},
+		
+	--各种背包
 	{
 		name = "",
-		label = "背包保鲜 󰀏",
-		hover = "",
+		label = " 󰀏 All Backpacks	背包保鲜效果 󰀏",
+		hover = "All Backpacks",
 		default = 0,
 		options = {
 			{description = "", data = 0},
@@ -117,90 +107,55 @@ configuration_options =
 	--普通背包
 	{
 		name = "backpack_config",
-		label = "普通背包",
+		label = "Backpack	普通背包",
 		hover = "Backpack spoilage rate config",
 		options =	{
-			{description = "关", data = false, hover = "Spoilage rate (OFF) Vanilla version"},
-			{description = "开", data = true, hover = "Spoilage rate (ON) Like fridge"},
+			{description = "默认", data = 0, hover = "Spoilage rate (OFF) Vanilla version"},
+			{description = "冰箱", data = 1, hover = "Spoilage rate (ON) Like fridge"},
+			{description = "熊包", data = 2, hover = "Spoilage rate (ON) Like Ice Pack"},
 		},
-		default = false,
+		default = 0,
 	},
-	--[[
-	{
-		name = "backpack_cooling_config",
-		label = "普通背包冷却",
-		hover = "Backpack cooling function",
-		options =	{
-			{description = "关", data = false, hover = "Ice Box cooling function (OFF)"},
-			{description = "开", data = true, hover = "Ice Box cooling function (ON)"},
-		},
-		default = false,
-	},
-	]]--
 	--小猪包
 	{
 		name = "piggyback_config",
-		label = "小猪包",
+		label = "Piggyback	小猪包",
 		hover = "Piggyback spoilage rate config",
 		options =	{
-			{description = "关", data = false, hover = "Spoilage rate (OFF) Vanilla version"},
-			{description = "开", data = true, hover = "Spoilage rate (ON) Like fridge"},
+			{description = "默认", data = 0, hover = "Spoilage rate (OFF) Vanilla version"},
+			{description = "冰箱", data = 1, hover = "Spoilage rate (ON) Like fridge"},
+			{description = "熊包", data = 2, hover = "Spoilage rate (ON) Like Ice Pack"},
 		},
-		default = false,
+		default = 0,
 	},
-	
+	--隔热包
+	{
+		name = "icepack_config",
+		label = "Ice Pack	隔热背包",
+		hover = "Ice Pack spoilage rate config",
+		options =	{
+			{description = "默认", data = 0, hover = "Spoilage rate (OFF) Vanilla version"},
+			{description = "冰箱", data = 1, hover = "Spoilage rate (ON) Like fridge"},
+		},
+		default = 0,
+	},
 	--小偷包
 	{
 		name = "krampus_sack_config",
-		label = "坎普斯包",
+		label = "Krampus sack	坎普斯包",
 		hover = "Krampus sack spoilage rate function",
 		options =	{
-			{description = "关", data = false, hover = "Spoilage rate (OFF) Vanilla version"},
-			{description = "开", data = true, hover = "Spoilage rate (ON) Like fridge"},
+			{description = "默认", data = 0, hover = "Spoilage rate (OFF) Vanilla version"},
+			{description = "冰箱", data = 1, hover = "Spoilage rate (ON) Like fridge"},
+			{description = "熊包", data = 2, hover = "Spoilage rate (ON) Like Ice Pack"},
 		},
-		default = false,
-	},
-	--[[
-	{
-		name = "krampus_sack_cooling_config",
-		label = "坎普斯包冷却",
-		hover = "Krampus sack cooling function",
-		options =	{
-			{description = "关", data = false, hover = "Ice Box cooling function (OFF)"},
-			{description = "开", data = true, hover = "Ice Box cooling function (ON)"},
-		},
-		default = false,
-	},
-	]]--
-	
-	--[[
-	--隔热包
-		{
-		name = "icepack_config",
-		label = "隔热背包",
-		hover = "Ice Box spoilage rate config",
-		options =	{
-			{description = "关", data = false, hover = "Spoilage rate (OFF) Vanilla version"},
-			{description = "开", data = true, hover = "Spoilage rate (ON) Like fridge"},
-		},
-		default = true,
-	},
-	{
-		name = "icepack_cooling_config",
-		label = "隔热背包冷却",
-		hover = "Ice Box cooling function",
-		options =	{
-			{description = "关", data = false, hover = "Ice Box cooling function (OFF)"},
-			{description = "开", data = true, hover = "Ice Box cooling function (ON)"},
-		},
-		default = false,
-	},
-	]]--
+		default = 0,
+	},	
 	
 	--骨灰盒
 	{
 		name = "",
-		label = "骨灰盒 󰀏",
+		label = "󰀏 Sisturn	骨灰盒 󰀏",
 		hover = "Sisturn",
 		default = 0,
 		options = {
@@ -209,7 +164,7 @@ configuration_options =
 	},
 	{
 		name = "sisturn_config",
-		label = "姐妹骨灰盒",
+		label = "spoilage rate config	姐妹骨灰盒",
 		hover = "Sisturn spoilage rate config",
 		options =	{
 			{description = "关", data = false, hover = "Spoilage rate (OFF) Vanilla version"},
@@ -217,11 +172,10 @@ configuration_options =
 		},
 		default = false,
 	},
-	
-		--烹饪锅
+	--烹饪锅
 	{
 		name = "",
-		label = "烹饪锅 󰀏",
+		label = "󰀏 CookingPot	烹饪锅 󰀏",
 		hover = "CookingPot",
 		default = 0,
 		options = {
@@ -230,19 +184,19 @@ configuration_options =
 	},
 	{
 		name = "cookingPot_config",
-		label = "烹饪锅",
+		label = "spoilage rate config	烹饪锅",
 		hover = "CookingPot spoilage rate config",
 		options =	{
 			{description = "关", data = false, hover = "Spoilage rate (OFF) Vanilla version"},
-			{description = "开", data = true, hover = "Spoilage rate (ON) Like fridge"},
+			{description = "开", data = true, hover = "Spoilage rate (ON) Like fridge (only work for those uncooked food)"},
 		},
 		default = false,
 	},
 	
-		--茶几
+	--茶几
 	{
 		name = "",
-		label = "茶几 󰀏",
+		label = "󰀏 EndTable	茶几 󰀏",
 		hover = "EndTable",
 		default = 0,
 		options = {
@@ -251,7 +205,7 @@ configuration_options =
 	},
 	{
 		name = "endTable_config",
-		label = "茶几",
+		label = "spoilage rate config	茶几",
 		hover = "EndTable spoilage rate config",
 		options =	{
 			{description = "关", data = false, hover = "Spoilage rate (OFF) Vanilla version"},
--- modmain.lua
+++ modmain.lua
@@ -1,92 +1,83 @@
-local backpack = GetModConfigData("backpack_config");
---local backpack_cooling = GetModConfigData("backpack_cooling_config");
 
-local piggyback = GetModConfigData("piggyback_config");
---local piggyback_cooling = GetModConfigData("piggyback_cooling_config");
-
-local krampus_sack = GetModConfigData("krampus_sack_config");
---local krampus_sack_cooling = GetModConfigData("krampus_sack_cooling_config");
-
-local icepack = GetModConfigData("icepack_config");
---local icepack_cooling = GetModConfigData("icepack_cooling_config");
-
-local sisturn = GetModConfigData("sisturn_config");
-
-local cookpot = GetModConfigData("cookingPot_config")
-
-local endTable = GetModConfigData("endTable_config")
-
---修改覆盖 tuning values !
-	TUNING.PERISH_MUSHROOM_LIGHT_MULT = GetModConfigData("mushroom_light_config");
-	
---冰箱	（待做： 分离冰箱和熊包的单独控制）
-	TUNING.PERISH_FRIDGE_MULT = GetModConfigData("icebox_config");
-	--TUNING.PERISH_COLD_FROZEN_MULT = GetModConfigData("icebox_config");
+--冰箱
+	TUNING.PERISH_FRIDGE_MULT = GetModConfigData("fridge_config");
 	
 --盐罐
 	TUNING.PERISH_SALTBOX_MULT = GetModConfigData("saltbox_config");
 	
---厨师包
-	--TUNING.PERISH_FOOD_PRESERVER_MULT 
+-- 蘑菇灯
+	TUNING.PERISH_MUSHROOM_LIGHT_MULT = GetModConfigData("mushroom_light_config");
+
+-- 背包
+	local backpack = GetModConfigData("backpack_config");
 	
---地面物体(宠物/鸟) 需要测试!!!!!!!
-	--TUNING.PERISH_GROUND_MULT
+	AddPrefabPostInit("backpack", function(inst)
+		if backpack ~= 0 then
+			inst:AddTag("fridge")
+			if backpack == 2 then
+				inst:AddTag("nocool")
+			end
+		end
+	end)
 	
---打湿
-	--TUNING.PERISH_WET_MULT
+-- 小猪包
+	local piggyback = GetModConfigData("piggyback_config");
+	
+	AddPrefabPostInit("piggyback", function(inst)
+		if piggyback ~= 0 then
+			inst:AddTag("fridge")
+			if piggyback == 2 then
+				inst:AddTag("nocool")
+			end
+		end
+	end)
 
---普通背包
-AddPrefabPostInit("backpack", function(inst)
-	if backpack then
-		inst:AddTag("fridge")
-	end
-end)
 
---小猪包
-AddPrefabPostInit("piggyback", function(inst)
-	if piggyback then
-		inst:AddTag("fridge")
-	end
-end)
+-- 隔热背包
+	local icepack = GetModConfigData("icepack_config");
 
---小偷包
-AddPrefabPostInit("krampus_sack", function(inst)
-	if krampus_sack then
-		inst:AddTag("fridge")
-	end
-end)
+	AddPrefabPostInit("icepack", function(inst)
+		if icepack == 1 then
+			inst:RemoveTag("nocool")
+		end
+	end)
 
---隔热包
---[[
-AddPrefabPostInit("icepack", function(inst)
-	inst:RemoveTag("fridge")
-	inst:RemoveTag("nocool")
-	if icepack then
-		inst:AddTag("fridge")
-	end
-	if not icepack_cooling then
-		inst:AddTag("nocool")
-	end
-end)
-]]--
+-- 小偷包
+	local krampus_sack = GetModConfigData("krampus_sack_config");
+
+	AddPrefabPostInit("krampus_sack", function(inst)
+		if krampus_sack ~= 0 then
+			inst:AddTag("fridge")
+			if krampus_sack == 2 then
+				inst:AddTag("nocool")
+			end
+		end
+	end)
 
 --骨灰盒
-AddPrefabPostInit("sisturn", function(inst)
-	if sisturn then
-		inst:AddTag("fridge")
-	end
-end)
+	local sisturn = GetModConfigData("sisturn_config");
+
+	AddPrefabPostInit("sisturn", function(inst)
+		if sisturn then
+			inst:AddTag("fridge")
+		end
+	end)
 
 --烹饪锅
-AddPrefabPostInit("cookpot", function(inst)
-	if cookpot then
-		inst:AddTag("fridge")
-	end
-end)
+	local cookpot = GetModConfigData("cookingPot_config")
+
+	AddPrefabPostInit("cookpot", function(inst)
+		if cookpot then
+			inst:AddTag("fridge")
+		end
+	end)
 
 --茶几
-AddPrefabPostInit("endtable", function(inst)
-	if endTable then
-		inst:AddTag("fridge")
-	end
-end)
+	local endTable = GetModConfigData("endTable_config")
+
+	AddPrefabPostInit("endtable", function(inst)
+		if endTable then
+			inst:AddTag("fridge")
+		end
+	end)
+
