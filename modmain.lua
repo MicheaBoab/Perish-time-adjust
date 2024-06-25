9ad4fbd1539a99ae800a363bf9a4f698294a86b5
Bozheng Liu <lb12016297@gmail.com>
2024-06-25 14:02:24 -0500

    Added wormlight as well

--- modmain.lua
+++ modmain.lua
@@ -1,15 +1,29 @@
-
---冰箱
+--	冰箱
 	TUNING.PERISH_FRIDGE_MULT = GetModConfigData("fridge_config");
 	
---盐罐
+--	盐罐
 	TUNING.PERISH_SALTBOX_MULT = GetModConfigData("saltbox_config");
+	
+-- 盐晶
+	TUNING.SALTROCK_PRESERVE_PERCENT_ADD = GetModConfigData("saltrock_config");
+	AddPrefabPostInit("lightbulb", function(inst)
+		inst:AddComponent("cookable")
+	end)
+	AddPrefabPostInit("wormlight", function(inst)
+		inst:AddComponent("cookable")
+	end)
+	AddPrefabPostInit("wormlight_lesser", function(inst)
+		inst:AddComponent("cookable")
+	end)
 
---	
+--	极地熊獾桶
 	TUNING.BEARGERFUR_SACK_PRESERVER_RATE = GetModConfigData("beargerfur_sack_config")
 	
 -- 蘑菇灯
 	TUNING.PERISH_MUSHROOM_LIGHT_MULT = GetModConfigData("mushroom_light_config");
+	
+-- 种子袋
+	TUNING.SEEDPOUCH_PRESERVER_RATE = GetModConfigData("seedpouch_config")
 
 -- 背包
 	local backpack = GetModConfigData("backpack_config");
@@ -37,13 +51,58 @@
 		end
 	end)
 
-
 -- 隔热背包
 	local icepack = GetModConfigData("icepack_config");
 
 	AddPrefabPostInit("icepack", function(inst)
 		if icepack == 1 then
 			inst:RemoveTag("nocool")
+		end
+	end)
+
+-- 种子包
+	local seedpouch = GetModConfigData("seedpouch_config");
+
+	AddPrefabPostInit("seedpouch", function(inst)
+		if seedpouch == 1 then
+			inst:AddTag("fridge")
+		end
+		if seedpouch == 2 then
+			inst:AddTag("fridge")
+			inst:AddTag("nocool")
+		end
+	end)
+	
+-- 威尔逊 胡子
+local beard_sack = GetModConfigData("beard_sack_config");
+
+	AddPrefabPostInit("beard_sack_1", function(inst)
+		if beard_sack == 1 then
+			inst:AddTag("fridge")
+		end
+		if beard_sack == 2 then
+			inst:AddTag("fridge")
+			inst:AddTag("nocool")
+		end
+	end)
+	
+	AddPrefabPostInit("beard_sack_2", function(inst)
+		if beard_sack == 1 then
+			inst:AddTag("fridge")
+		end
+		if beard_sack == 2 then
+			inst:AddTag("fridge")
+			inst:AddTag("nocool")
+		end
+	end)
+	
+	AddPrefabPostInit("beard_sack_3", function(inst)
+		if beard_sack == 1 then
+			inst:AddTag("fridge")
+		end
+		if beard_sack == 2 then
+			inst:AddTag("fridge")
+			inst:AddTag("nocool")
 		end
 	end)
 
@@ -97,3 +156,4 @@
 			inst:AddTag("fridge")
 		end
 	end)
+
